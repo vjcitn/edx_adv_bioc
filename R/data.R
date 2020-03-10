2d0ae8aef8cb4b28c393fe9bc88849bfc79d50d2
Vince Carey <stvjc@channing.harvard.edu>
2020-03-10 07:49:59 -0400

    adding serializations for windows

--- R/data.R
+++ R/data.R
@@ -2,3 +2,18 @@
 #' @docType data
 #' @format character vector
 "gopw_selected"
+
+#' a selection of CREB1-targeted ChIP-seq 'signal p-value' reports in a GRangesList
+#' @docType data
+#' @format GRangesList
+"creb1_sels"
+
+#' output of import_enc_bw with CREB1-targeted ChIP-seq 'signal p-value' reports
+#' @docType data
+#' @format GRangesList
+"skd"
+
+#' output of import_enc_bw with FOXA1-targeted ChIP-seq 'signal p-value' reports
+#' @docType data
+#' @format GRangesList
+"uu"
